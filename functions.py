04293d2b74612acbfc7c38a88830b74ba21e59ba
Morgaine <morgaine@shareenas-air.home>
2023-11-16 10:33:47 +0000

    tidied up functions

--- functions.py
+++ functions.py
@@ -15,24 +15,20 @@ options.headless = True
 
 driver = webdriver.Chrome(options=options)
 
-timeout = 30
 
-def fetch_title(url):
+def fetch_title(timeout):
     try: 
-        title_element_exists = WebDriverWait(driver, timeout).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'h1.hpipapi')), 'Title element not found within timeout') 
+        title_element_exists = WebDriverWait(driver, timeout).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'h1.hpipapi.i1pmzyw7.dir.dir-ltr')), 'Title element not found within timeout') 
+        print(title_element_exists.get_attribute('innerHTML'))
 
-        if title_element_exists: #THIS IS STATEFUL REMOVE IT
-            print(title_element_exists.get_attribute('innerHTML'))
-        else:
-            print('cannot find title element')
     except Exception as e:
         print(e)
 
 
-def fetch_house_type(url):
+def fetch_house_type(timeout):
 #try to fetch type of house element
     try: 
-        type_element_exists = WebDriverWait(driver, timeout).until( EC.presence_of_element_located((By.TAG_NAME, 'h2.hpipapi')), 'Type of house element not found within timeout')
+        type_element_exists = WebDriverWait(driver, timeout).until( EC.presence_of_element_located((By.CSS_SELECTOR, 'div.toieuka.dir.dir-ltr h1')), 'Type of house element not found within timeout')
         if type_element_exists:
                 typeString = type_element_exists.get_attribute('innerHTML')
 
@@ -51,7 +47,7 @@ def fetch_house_type(url):
         print(e)
 
 
-def fetch_bathrooms_bedrooms(url):
+def fetch_bathrooms_bedrooms(timeout):
     #try to fetch bathroom and bedroom elements
     try:
         rooms = EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'span.pen26si.dir.dir-ltr + span'))
@@ -65,7 +61,7 @@ def fetch_bathrooms_bedrooms(url):
         print(e)
 
 
-def fetch_amenities(url):
+def fetch_amenities(timeout):
     #try to fetch amenties
     try: 
         #find the amenities button by start and end of text of button and click it to create and launch the modal
